aee708e79b7c456a65113012179a510800c159d3
HubiBoar <150520704+HubiBoar@users.noreply.github.com>
2025-04-14 16:24:04 +0000

    main.go init

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,2 +1,12 @@
+// based on https://github.com/charmbracelet/bubbletea-app-template
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
